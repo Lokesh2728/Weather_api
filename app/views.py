6fca1f54ed7428b02a1feaed8405eb9a77e88569
Lokesh Patel <101927859+Lokesh2728@users.noreply.github.com>
2025-05-28 20:34:43 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -36,14 +36,6 @@ def home(request):
             'wind_speed':wind_speed
         }
 
-
-
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-                ip = x_forwarded_for.split(',')[0]
-        else:
-                ip = request.META.get('REMOTE_ADDR')
-            
         print(f"User IP address is: {ip}")
         return render(request,'home.html',d)
     return render(request,'home.html',d)
